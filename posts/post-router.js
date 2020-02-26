662b56e64803fb127282baa93796c48773be13e3
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-02-25 21:41:51 -0500

    lecture complete

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,26 +1,75 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require('express');
+const db = require('../data/config');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  try {
+    // translates to SELECT * from posts
+    const posts = await db.select('*').from('posts');
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.get('/:id', async (req, res, next) => {
+  try {
+    const posts = await db
+      .first('*')
+      .from('posts')
+      .where('id', req.params.id);
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents
+    };
+    // translates to INSERT INTO posts {title, contents} VALUES (?, ?)
+    // await db.insert(payload).into('posts'); or....
+    const { id } = await db('posts').insert(payload);
+    const newPost = await db('posts')
+      .where('id', id)
+      .first();
+    res.json(newPost);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.put('/:id', async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents
+    };
+    await db('posts')
+      .where('id', req.params.id)
+      .update(payload);
+    const post = await db('posts')
+      .where('id', req.params.id)
+      .first();
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/", async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await db('posts')
+      .where('id', req.params.id)
+      .del();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
-
-router.put("/:id", async (req, res, next) => {
-
-})
-
-router.delete("/:id", async (req, res, next) => {
-
-})
-
-module.exports = router
+module.exports = router;
